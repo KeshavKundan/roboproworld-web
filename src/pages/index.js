316293fb20893963c2d687617787e3903d9e6238
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-03-09 08:42:08 +0530

    modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import NavBar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <NavBar/>
             <main className={styles.main}>
                 <div className={styles.description}>
                     <p>
